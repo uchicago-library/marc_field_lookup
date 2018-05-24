18133485965e01aaf832e8a55cf0cbe7a5b18c74
verbalhanglider <tdanstrom@uchicago.edu>
2018-05-24 12:49:18 -0500

    fixed typo marc_schema.json to marc-schema.json

--- marclookup/__init__.py
+++ marclookup/__init__.py
@@ -2,9 +2,5 @@ import pkg_resources
 import json
 from os.path import abspath, exists
 
-DATA = abspath(pkg_resources.resource_filename(__name__, "marc_schema.json"))
-
-print(exists(DATA))
-
+DATA = abspath(pkg_resources.resource_filename(__name__, "marc-schema.json"))
 DATA = json.load(open(DATA, 'r'))
-
