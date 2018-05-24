95fdaab79e292241b6a25bcd0bebb07ad46ceb54
verbalhanglider <tdanstrom@uchicago.edu>
2018-05-24 12:47:15 -0500

    changed file look up __init__ rm data dir

--- marclookup/__init__.py
+++ marclookup/__init__.py
@@ -2,7 +2,7 @@ import pkg_resources
 import json
 from os.path import abspath, exists
 
-DATA = abspath(pkg_resources.resource_filename(__name__, "data/marc_schema.json"))
+DATA = abspath(pkg_resources.resource_filename(__name__, "marc_schema.json"))
 
 print(exists(DATA))
 
