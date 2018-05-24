c7dc391efd100ebf4c4151285d5198cc5859d09c
verbalhanglider <tdanstrom@uchicago.edu>
2018-05-24 12:34:14 -0500

    added import for os.path.exists

--- marclookup/__init__.py
+++ marclookup/__init__.py
@@ -1,6 +1,6 @@
 import pkg_resources
 import json
-from os.path import abspath
+from os.path import abspath, exists
 
 DATA = abspath(pkg_resources.resource_filename(__name__, "data/marc_schema.json"))
 
